0c36ef3616987ff9cede7604884a805e9d25ce52
ALLAN CEDRIC GOUVEA BRUGLIMANN ALVES DA SILVA <acgbas19@urquell>
2020-03-12 23:34:49 -0300

    Primeiro trabalho : parte 1.1 - carregamento do dicionário

--- verificador_ortografico/dicionario.c
+++ verificador_ortografico/dicionario.c
@@ -8,6 +8,7 @@ int TAM_DICIO = 0 ;
 
 void carrega_dicionario (FILE* arq, dicio_t *d)
 {
+
    d->palavra = aloca_dicionario (Q_PALAVRAS) ;
    if (!d->palavra)
    {
@@ -17,10 +18,14 @@ void carrega_dicionario (FILE* arq, dicio_t *d)
 
    aloca_caracteres (d, 0) ;
 
+   /* Aloca cada palavra do dicionário */
    int i = 0 ;
    while ( fgets (d->palavra[i], 50, arq) )
    {
+      /* Remove o caractere '\n' lido pelo fgets */   
       d->palavra[i][strcspn (d->palavra[i],"\n")] = 0 ;
+
+      /* Limita o índice para não acessar posições de memória desconhecidas */
       if ( i < Q_PALAVRAS-1 )
 	      i++ ;
    }
@@ -34,6 +39,7 @@ void *aloca_dicionario (int tam)
    return (malloc (sizeof(char *)*tam)) ;
 }
 
+/* Em testes */
 void *realoca_dicionario (dicio_t *d, int tam)
 {
    return (realloc (d->palavra, sizeof(char *)*tam)) ;
@@ -45,6 +51,7 @@ void aloca_caracteres (dicio_t *d ,int ini)
 
    for (i = ini; i < Q_PALAVRAS; i++)
    {
+      /* Margem segura de 50 caracteres p/ cada palavra*/
       d->palavra[i] = malloc (sizeof(char)*50) ;
 
       if (!d->palavra[i])
@@ -63,6 +70,7 @@ void desaloca_dicionario (dicio_t *d)
    free (d->palavra) ;
 }
 
+/* Função teste */
 void impressao_dicionario (dicio_t *d)
 {
 	int i ;
