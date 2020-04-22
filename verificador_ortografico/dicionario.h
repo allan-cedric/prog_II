d233679226396ba44fd023612976a4b0111d35b6
allan-cedric <allan.py3000@gmail.com>
2020-04-22 16:54:12 -0300

    Protótipos de funções

--- verificador_ortografico/dicionario.h
+++ verificador_ortografico/dicionario.h
@@ -16,7 +16,6 @@
 /*
   Defines
 */
-
 #define MAX_ALOC 20000
 #define TAM_PALAVRA 50
 
@@ -25,47 +24,44 @@
 */
 struct dicio_t
 {
-   unsigned char **palavra ;
-   int tam_dicio ; /* Número de palavras */
-   int tam_aloc ;  /* Quantidade de alocações */
-} ;
-typedef struct dicio_t dicio_t ;
+  unsigned char **palavra;
+  int tam_dicio; /* Número de palavras */
+  int tam_aloc;  /* Quantidade de alocações */
+};
+typedef struct dicio_t dicio_t;
 
 /*
    Protótipos de funções de alocação do dicionário
 */
-void carrega_dicionario (FILE *arq, dicio_t *d) ;
+void carrega_dicionario(FILE *arq, dicio_t *d);
 
-void *aloca_palavras (dicio_t *d) ;
+void *aloca_palavras(dicio_t *d);
 
-void *realoca_palavras (dicio_t *d) ;
+void *realoca_palavras(dicio_t *d);
 
-void aloca_caracteres (dicio_t *d, int ini, int tam) ;
+void aloca_caracteres(dicio_t *d, int ini, int tam);
 
-void desaloca_dicionario (dicio_t *d) ;
+void desaloca_dicionario(dicio_t *d);
+
 /*
   Protótipos de funções de identificação de caracteres
   e de busca no dicionário
 */
+int eh_letra(unsigned char ch);
 
-int eh_letra (unsigned char ch) ;
-
-int bsearch_dicionario (unsigned char *p, dicio_t *d) ;
+int bsearch_dicionario(unsigned char *p, dicio_t *d);
 
 /* (UNSIGNED) Verifica se a string *p é maior, menor ou igual a string *p1 (Ordem lexicográfica) */
-int ustrcmp (unsigned char *p, unsigned char *p1) ;
-
-/* (UNSIGNED) Tamanho da string *p */
-int ustrlen (unsigned char *p) ;
+int ustrcmp(unsigned char *p, unsigned char *p1);
 
 /*
   Protótipos de funções de alteração de caracteres
 */
-unsigned char *minuscula (unsigned char *s) ;
+unsigned char *minuscula(unsigned char *s);
 
 /*
   Função teste de impressão
 */
-void impressao_dicionario (dicio_t *d) ;
+void impressao_dicionario(dicio_t *d);
 
 #endif
